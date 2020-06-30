f4a4e89fc3e2c5b1605a1fcc5ed05759a9fb2ed8
TxrWall <wllcblm@gmail.com>
2020-06-30 10:03:05 -0300

    Testing  props & user interaction

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="app-header">
     <button v-show="loggedIn">Logout</button>
   </div>
 </template>
@@ -14,4 +14,14 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.app-header {
+  min-height: 40px;
+  padding: 0.2em 1em;
+  background: white;
+}
+
+button {
+  float: right;
+}
+</style>
--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -2,12 +2,12 @@ import AppHeader from '@/components/AppHeader'
 import { mount } from '@vue/test-utils'
 
 describe('AppHeader', () => {
-  test('If user is not logged in, do not show logout button', () => {
+  test('if user is not logged in, do not show logout button', () => {
     const wrapper = mount(AppHeader)
     expect(wrapper.find('button').isVisible()).toBe(false)
   })
 
-  test('If user is logged in, show logout button', async () => {
+  test('if logged in, show logout button', async () => {
     const wrapper = mount(AppHeader)
     wrapper.setData({ loggedIn: true })
 
